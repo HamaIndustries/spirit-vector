b927ac952ddbcd214dd153f1f8078fd79dce4001
unknown <eliatlarge@gmail.com>
2024-06-11 21:01:36 -0500

    initial

---
+++ src/client/java/symbolics/division/spirit/vector/DreamBurstSpiritVectorClient.java
@@ -0,0 +1,10 @@
+package symbolics.division.spirit.vector;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DreamBurstSpiritVectorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/symbolics/division/spirit/vector/DreamBurstSpiritVectorDataGenerator.java
@@ -0,0 +1,11 @@
+package symbolics.division.spirit.vector;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DreamBurstSpiritVectorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
