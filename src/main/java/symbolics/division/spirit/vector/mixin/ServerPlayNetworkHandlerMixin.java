dc7623444465d44fc544f625ff99ec6837126f30
eliatlarge <eliatlarge@gmail.com>
2024-07-23 23:19:18 -0700

    skip teleport check for SV havers. pls don't abuse ;-;

---
+++ src/main/java/symbolics/division/spirit/vector/mixin/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,18 @@
+package symbolics.division.spirit.vector.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import symbolics.division.spirit.vector.logic.SpiritVector;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class ServerPlayNetworkHandlerMixin {
+    @ModifyExpressionValue(
+            method = "onPlayerMove",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isInTeleportationState()Z")
+    )
+    public boolean lieToServerThatWeAreTeleportingToPreventRubberbanding(boolean actuallyTeleporting) {
+        return actuallyTeleporting || SpiritVector.hasEquipped(((ServerPlayNetworkHandler)(Object)this).player);
+    }
+}
